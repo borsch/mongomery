26295a49c4fe8880d326191cab6cd183fd2ac4df
Oleh Kurpiak <oleh.kurpiak@gmail.com>
2020-01-22 16:26:49 +0200

    use junit5 jupiter as test engine

--- src/test/java/com/github/borsch/mongomery/TestMongoDBTester.java
+++ src/test/java/com/github/borsch/mongomery/TestMongoDBTester.java
@@ -9,10 +9,10 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
 
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
@@ -21,13 +21,13 @@ import de.flapdoodle.embed.mongo.distribution.Version;
 import de.flapdoodle.embed.mongo.tests.MongodForTestsFactory;
 import net.minidev.json.JSONObject;
 
-public class TestMongoDBTester {
+class TestMongoDBTester {
 
     private static MongoDBTester mongoDBTester;
     private static MongodForTestsFactory factory = null;
 
-    @BeforeClass
-    public static void init() throws IOException {
+    @BeforeAll
+    static void init() throws IOException {
         factory = MongodForTestsFactory.with(Version.Main.V3_3);
 
         final MongoClient mongo = factory.newMongo();
@@ -35,26 +35,26 @@ public class TestMongoDBTester {
         mongoDBTester = new MongoDBTester(db, "/expected/", "/predefined/");
     }
 
-    @Before
-    public void beforeMethod() {
+    @BeforeEach
+    void beforeMethod() {
         mongoDBTester.cleanIgnorePath();
         mongoDBTester.dropDataBase();
     }
 
-    @AfterClass
-    public static void shutdown() {
+    @AfterAll
+    static void shutdown() {
         if (factory != null)
             factory.shutdown();
     }
 
     @Test
-    public void strictMatchSimplestTestShouldPass() {
+    void strictMatchSimplestTestShouldPass() {
         mongoDBTester.setDBState("strictMatch/simplestTestDataSet.json");
         mongoDBTester.assertDBStateEquals("strictMatch/simplestTestShouldPassExpectedDataSet.json");
     }
 
     @Test
-    public void strictMatchSimplestTestShouldFail() {
+    void strictMatchSimplestTestShouldFail() {
         mongoDBTester.setDBState("strictMatch/simplestTestDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("strictMatch/simplestTestShouldFailExpectedDataSet.json"))
             .isInstanceOf(AssertionError.class)
@@ -62,19 +62,19 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void patternMatchOnlySimplestTestFor$AnyObjectShouldPass() {
+    void patternMatchOnlySimplestTestFor$AnyObjectShouldPass() {
         mongoDBTester.setDBState("patternMatch/simplestTest$anyObjectDataSet.json");
         mongoDBTester.assertDBStateEquals("patternMatch/simplestTest$anyObjectDataSet.json");
     }
 
     @Test
-    public void simplestTest$anyObjectMixedWith$anyString() {
+    void simplestTest$anyObjectMixedWith$anyString() {
         mongoDBTester.setDBState("patternMatch/simplestTest$anyObjectMixedWith$anyStringDataSet.json");
         mongoDBTester.assertDBStateEquals("patternMatch/simplestTest$anyObjectMixedWith$anyStringDataSet.json");
     }
 
     @Test
-    public void patternMatch$anyObjectShouldFailOnNull() {
+    void patternMatch$anyObjectShouldFailOnNull() {
         mongoDBTester.setDBState("patternMatch/simplestTest$PlaceholderCantBeNullDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/simplestTest$anyObjectCantBeNullDataSet.json"))
             .isInstanceOf(AssertionError.class)
@@ -82,7 +82,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void patternMatch$anyStringShouldFailOnNull() {
+    void patternMatch$anyStringShouldFailOnNull() {
         mongoDBTester.setDBState("patternMatch/simplestTest$PlaceholderCantBeNullDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/simplestTest$anyStringCantBeNullDataSet.json"))
             .isInstanceOf(AssertionError.class)
@@ -90,13 +90,13 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void complexTestShouldPass() {
+    void complexTestShouldPass() {
         mongoDBTester.setDBState("patternMatch/complexTestDataSet.json");
         mongoDBTester.assertDBStateEquals("patternMatch/complexTestDataSet.json");
     }
 
     @Test
-    public void complexTestShouldFailIfCantFindAllStrictMatches() {
+    void complexTestShouldFailIfCantFindAllStrictMatches() {
         mongoDBTester.setDBState("patternMatch/complexTestDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/complexTestShouldFailIfCantFindAllStrictMatches.json"))
             .isInstanceOf(AssertionError.class)
@@ -104,7 +104,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void complexTestShouldFailIfCantFindAllPatternMatches() {
+    void complexTestShouldFailIfCantFindAllPatternMatches() {
         mongoDBTester.setDBState("patternMatch/complexTestDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/complexTestShouldFailIfCantFindAllPatternMatches.json"))
             .isInstanceOf(AssertionError.class)
@@ -112,7 +112,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldFailIfUserUsesPlaceholderMixedWithCharInString() {
+    void shouldFailIfUserUsesPlaceholderMixedWithCharInString() {
         final JSONObject jsonObject = new JSONObject();
         jsonObject.put("_id", "$anyObject()");
         jsonObject.put("email", " $anyString()");
@@ -124,7 +124,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldCompareLongViaEqualPatternMatch() {
+    void shouldCompareLongViaEqualPatternMatch() {
         final JSONObject jsonObject = new JSONObject();
         jsonObject.put("fieldLongValue", 12345623534L);
         mongoDBTester.setDBState("TestCollection", jsonObject);
@@ -132,7 +132,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldFailToCompareLongViaEqualPatternMatchWhenValuesAreDifferent() {
+    void shouldFailToCompareLongViaEqualPatternMatchWhenValuesAreDifferent() {
         final JSONObject jsonObject = new JSONObject();
         jsonObject.put("fieldLongValue", 1L);
         mongoDBTester.setDBState("TestCollection", jsonObject);
@@ -142,7 +142,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldCompareLongViaAnyPatternMatch() {
+    void shouldCompareLongViaAnyPatternMatch() {
         final JSONObject jsonObject = new JSONObject();
         jsonObject.put("fieldLongValue", 12345623534L);
         mongoDBTester.setDBState("TestCollection", jsonObject);
@@ -150,7 +150,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldFailIfPlaceholderIsInsideArray() {
+    void shouldFailIfPlaceholderIsInsideArray() {
         final InputStream fileStream = this.getClass()
                 .getResourceAsStream("/predefined/patternMatch/placeholderCantBeInsideArray.json");
         mongoDBTester.setDBState(fileStream);
@@ -160,7 +160,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void simplestTestShouldFail$anyObjectDataSet() {
+    void simplestTestShouldFail$anyObjectDataSet() {
         mongoDBTester.setDBState("patternMatch/simplestTestShouldFail$anyObjectDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/simplestTestShouldFail$anyObjectDataSet.json"))
             .isInstanceOf(AssertionError.class)
@@ -168,19 +168,19 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void advancedPatternMatchShouldPass() {
+    void advancedPatternMatchShouldPass() {
         mongoDBTester.setDBState("advancedPatternMatch/testShouldPass$anyStringDataSet.json");
         mongoDBTester.assertDBStateEquals("advancedPatternMatch/testShouldPass$anyStringDataSet.json");
     }
 
     @Test
-    public void testShouldPass$anyObjectDataSet() {
+    void testShouldPass$anyObjectDataSet() {
         mongoDBTester.setDBState("advancedPatternMatch/testShouldPass$anyObjectDataSet.json");
         mongoDBTester.assertDBStateEquals("advancedPatternMatch/testShouldPass$anyObjectDataSet.json");
     }
 
     @Test
-    public void testShouldFail$anyObjectDataSet() {
+    void testShouldFail$anyObjectDataSet() {
         mongoDBTester.setDBState("advancedPatternMatch/testShouldPass$anyObjectDataSet.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("advancedPatternMatch/testShouldFail$anyObjectDataSet.json"))
             .isInstanceOf(AssertionError.class)
@@ -188,7 +188,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldFailWhenCollectionNamesAreDifferent() {
+    void shouldFailWhenCollectionNamesAreDifferent() {
         mongoDBTester.setDBState("TestCollection", new JSONObject());
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("differentCollectionNames.json"))
             .isInstanceOf(AssertionError.class)
@@ -196,7 +196,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldThrowException_whenCannotMatchStrictly() {
+    void shouldThrowException_whenCannotMatchStrictly() {
         mongoDBTester.setDBState("patternMatch/patternMatch_withStrictMatch.json");
         assertThatThrownBy(() -> mongoDBTester.assertDBStateEquals("patternMatch/patternMatch_withStrictMatch.json"))
             .isInstanceOf(AssertionError.class)
@@ -204,7 +204,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldMathAnyDate() {
+    void shouldMathAnyDate() {
         final JSONObject dbState = new JSONObject();
         dbState.put("date", new Date());
         dbState.put("localDate", LocalDate.now());
@@ -218,7 +218,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldPassStrictMatchWithIgnoreFields() {
+    void shouldPassStrictMatchWithIgnoreFields() {
         mongoDBTester.addIgnorePaths(
             "_id",
             "intField",
@@ -232,7 +232,7 @@ public class TestMongoDBTester {
     }
 
     @Test
-    public void shouldFailStrictMatchWithIgnoreFields() {
+    void shouldFailStrictMatchWithIgnoreFields() {
         mongoDBTester.addIgnorePaths(
             "_id",
             "intField",
